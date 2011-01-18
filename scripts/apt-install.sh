5f2cf9608d9f1dbc0069c661bee87e8fe16e2dcb
Seth Woodworth <seth@isforinsects.com>
2011-01-18 16:35:46 -0500

    minor upgrade to my install scripts, doesn't really work still

--- scripts/apt-install.sh
+++ scripts/apt-install.sh
@@ -6,7 +6,7 @@ sudo aptitude install vim-nox
 ## install my standard apps
 # TODO: Add more pkgs to install for default configs, this can't be everything I use.
 # TODO: Split this into media / prog tools / sys-tools
-sudo aptitude install audacity build-essential git-core gnome-do gpw inkscape markdown screen-profiles signing-party subversion synergy
+sudo aptitude install audacity build-essential git-core gpw inkscape signing-party subversion 
 
 # media tools
 sudo aptitude install gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly mpc mpd mplayer mplayer-nogui 
@@ -19,7 +19,7 @@ sudo aptitude install sqlite3
 
 ## set up my python env
 echo "You want to play with the python? Mkay, let's get you stuff you'll need for that."
-sudo aptitude install python2.5 python2.6 python-beautifulsoup python-crypto python-feedparser python-matplotlib python-networkx python-markdown python-pygraphviz python-rsvg python-sqlalchemy python-yaml 
+sudo aptitude install python2.6 python-beautifulsoup python-crypto python-feedparser python-matplotlib python-networkx python-markdown python-pygraphviz python-rsvg python-sqlalchemy python-yaml 
 
 ## set up ruby env
 # TODO: add commands here to install the rubygems I use.
