d996da301f22f27c6d224512ad173f163bf385ab
Topher Brown <topher200@gmail.com>
2011-02-14 00:06:38 -0500

    moving the encrypted password to the config file

--- gmail_to_db/gmail_to_db.py
+++ gmail_to_db/gmail_to_db.py
@@ -19,7 +19,7 @@ lock_dir = os.path.join(os.getcwd(), 'gmail_to_db.lck')
 config_filename = 'gmail_to_db.cfg'
 config_file = configobj.ConfigObj(config_filename)
 user = 'daphotline'
-password = base64.b64decode('ZmViMTEyMDEx')
+password = base64.b64decode(config_file['encrypted_password'])
 
 # Check if anyone has the file lock already
 try:
