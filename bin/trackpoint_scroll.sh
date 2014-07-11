98f85d52791220c71979336614de3bff2dfe9529
Seth Woodworth <seth@sethish.com>
2014-07-11 16:10:21 -0400

    remove linux+lenovo keyboard specific script

--- bin/trackpoint_scroll.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-LOOKFOR="Lite-On Technology Corp\. ThinkPad USB Keyboard with TrackPoint"
-for id in $(xinput list | egrep "${LOOKFOR}" | cut -f2 | cut -d= -f2); do
-    xinput set-int-prop $id "Evdev Wheel Emulation" 8 1
-    xinput set-int-prop $id "Evdev Wheel Emulation Button" 8 2
-done
