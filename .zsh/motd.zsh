5f5c6295714a9d0fc727e7160e1376350070082d
Seth Woodworth <seth@sethish.com>
2015-01-24 00:56:09 -0500

    making the debian swirl my login motd

--- .zsh/motd.zsh
+++ .zsh/motd.zsh
@@ -1,15 +1,2 @@
-# the batsymbol
-echo
-echo "$fg_no_bold[yellow]                     _==/          i     i          \\==_"
-echo "                   /XX/            |\___/|            \\XX\\"
-echo "                 /XXXX\\            |XXXXX|            /XXXX\\"
-echo "                |XXXXXX\\_         _XXXXXXX_         _/XXXXXX|"
-echo "               XXXXXXXXXXXxxxxxxxXXXXXXXXXXXxxxxxxxXXXXXXXXXXX"
-echo "              |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|"
-echo "              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-echo "              |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|"
-echo "               XXXXXX/^^^^\"\\XXXXXXXXXXXXXXXXXXXXX/^^^^^\\XXXXXX"
-echo "                |XXX|       \\XXX/^^\\XXXXX/^^\\XXX/       |XXX|"
-echo "                  \\XX\\       \\X/    \\XXX/    \\X/       /XX/"
-echo "                     \"\       \"      \\X/      \"      /\"  $reset_color"
-echo
+# debian
+linuxlogo
